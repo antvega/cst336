152a8490fbd014df94abde9a1c52b9d3f8c42f92
Antonio Vega <antvega@csumb.edu>
2018-08-27 22:22:48 +0000

    adding index.php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+function helloWorld() {
+    echo 'hello world';
+}
+
+helloWorld();
+
+?>
+</body>
+</html>
