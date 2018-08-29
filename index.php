94ebeac3f39e44b1babfc93afe6ed3fe36f78be3
Antonio Vega <antvega@csumb.edu>
2018-08-29 22:04:57 +0000

    Bones to lab 1

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Antonio's Web</title>
+    </head>
+    
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
