ab4eadc6f7dffa80bfeba771971eb84a26b7d4f6
Antonio Vega <antvega@csumb.edu>
2018-09-23 01:56:58 +0000

    777 updated

--- labs/777/index.php
+++ labs/777/index.php
@@ -2,27 +2,13 @@
 <html>
     <head>
         <title> 777 Slot Machine </title>
-        <style>
-            @import url("css/styles.css");
-        </style>
+
     </head>
     <body> 
         <!--<img src = "img/cherry.png" alt="cherry" title="Cherry" width="70"/>-->
-        <div id="main">
-            <form>
-                <input type="submit" value="Spin!"/>
-            </form>   
+        <div >
             <?php
-                include 'inc/functions.php';
-                play();
+                echo hello;
             ?>
         </div>
     </body>
-</html><html>
-    <head>
-        <title> 777 Slot Machine </title>
-    </head>
-    <body>
-
-    </body>
-</html>
